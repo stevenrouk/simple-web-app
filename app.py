370ebfe649af33e49a0bd53938151d3b2ab18f72
Steven Rouk <stevenrouk@gmail.com>
2019-10-21 17:42:56 -0600

    Change to correct port.

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=8105, threaded=True)
+    app.run(host='localhost', port=8080, threaded=True)
